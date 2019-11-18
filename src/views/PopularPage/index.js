1df5e2f11637178568025f0c90d568e40ccc5a47
zzzhim <3218566389@qq.com>
2019-11-18 19:19:30 +0800

    设置顶部导航

--- src/views/PopularPage/index.js
+++ src/views/PopularPage/index.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import { styles } from './style'
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs'
+import { createAppContainer } from 'react-navigation'
 
 export default class PopularPage extends Component {
     render() {
+        // 设置顶部导航
+        const TabNavigator = createAppContainer(createMaterialTopTabNavigator(
+            {
+                PopularTab1: {
+                    screen: PopularTab,
+                    navigationOptions: {
+                        title: 'Tab1'
+                    }
+                },
+                PopularTab2: {
+                    screen: PopularTab,
+                    navigationOptions: {
+                        title: 'Tab2'
+                    }
+                }
+            }
+        ))
+
         return (
             <View style={ styles.container }>
-                <Text style={ styles.welcome }> PopularPage </Text>
+                <TabNavigator />
+            </View>
+        )
+    }
+}
+
+class PopularTab extends Component {
+    render() {
+        return (
+            <View style={ styles.container }>
+                <Text>PopularPage</Text>
             </View>
         )
     }
